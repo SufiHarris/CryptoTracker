70bb60286e8d5bd90c3a9ddbee6c7c564318dc5d
SufiHarris <harrissofi02@gmail.com>
2024-02-24 14:54:10 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Mohammad Haris Sofi on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
